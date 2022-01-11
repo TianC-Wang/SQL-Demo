6003095e40990aa6ed36a54e9146ee80cb503746
TianC Wang <thewater2016@qq.com>
2022-01-11 16:52:42 +0800

    20220110 final

--- header/SQL-Demo.h
+++ header/SQL-Demo.h
@@ -2,9 +2,40 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 
 typedef struct sqlDemo_object
 {
+    size_t headersCount;
+    char** headers;
 } SQLDEMO_OBJECT;
 
+/// @brief Quickly compare two strings are same or not.
+/// @param _Str1 The first string.
+/// @param _Str2 The second string.
+/// @return Whether they are same.
+/// @remark Untested!
+bool sqlDemo_quickStrcmp(const char* _Str1, const char* _Str2);
+
+/// @brief Create an empty object.
+/// @return The object.
+/// @remark Updating!
 SQLDEMO_OBJECT* sqlDemo_new(void);
+
+/// @brief Dispose an object.
+/// @param _Obj The target object.
+/// @remark Updating!
+void sqlDemo_delete(SQLDEMO_OBJECT* _Obj);
+
+/// @brief Add a column to the end of the object.
+/// @param _Obj The target object.
+/// @param _Str The header's unique name.
+/// @return Whether success(, false if already exists).
+/// @remark Untested!
+bool sqlDemo_addHeader(SQLDEMO_OBJECT* _Obj, const char* _Str);
+
+/// @brief Output the object to a file.
+/// @param _Obj The target object.
+/// @param _Fs The target file stream.
+/// @remark Not implemented!
+void sqlDemo_output(SQLDEMO_OBJECT* _Obj, FILE* _Fs);
--- src/SQL-Demo.c
+++ src/SQL-Demo.c
@@ -1,9 +1,59 @@
 #include "../header/SQL-Demo.h"
 
+bool sqlDemo_quickStrcmp(const char *_Str1, const char *_Str2)
+{
+    if (_Str1 == NULL || _Str2 == NULL)
+        return false;
+    while (*_Str1 != 0 && *_Str2 != 0)
+    {
+        if (*_Str1 != *_Str2) return false;
+        _Str1++;
+        _Str2++;
+    }
+    return *_Str1 == 0 && *_Str2 == 0;
+}
+
 SQLDEMO_OBJECT* sqlDemo_new(void)
 {
     SQLDEMO_OBJECT* ret = malloc(sizeof(SQLDEMO_OBJECT));
-    if (ret == NULL) return NULL;
+    if (ret == NULL)
+        return NULL;
     memset(ret, 0, sizeof(SQLDEMO_OBJECT));
     return ret;
+}
+
+void sqlDemo_delete(SQLDEMO_OBJECT* _Obj)
+{
+    size_t i = 0;
+    if (_Obj != NULL)
+    {
+        for (i = 0; i < _Obj->headersCount; i++)
+            free(_Obj->headers[i]);
+        if (i > 0)
+            free(_Obj->headers);
+        free(_Obj);
+    }
+}
+
+bool sqlDemo_addHeader(SQLDEMO_OBJECT* _Obj, const char* _Str)
+{
+    char** pLegacy = NULL;
+    size_t i = 0;
+    if (_Obj != NULL)
+    {
+        for (i = 0; i < _Obj->headersCount; i++)
+            if (!sqlDemo_quickStrcmp(_Str, _Obj->headers[i]))
+                return false;
+        _Obj->headersCount++;
+        pLegacy = _Obj->headers;
+        _Obj->headers = malloc(sizeof(char *) * _Obj->headersCount);
+        for (i = 0; i < _Obj->headersCount - 1; i++)
+            _Obj->headers[i] = pLegacy[i];
+        _Obj->headers[i] = malloc(sizeof(char) * (strlen(_Str) + 1));
+        strcpy(_Obj->headers[i], _Str);
+    }
+}
+
+void sqlDemo_output(SQLDEMO_OBJECT* _Obj, FILE *_Fs)
+{
 }
